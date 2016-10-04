5c70b57de91906ae1701448d211c80d7e8c67571
Rosenny Griffin <rosenny08@gmail.com>
2016-10-04 10:03:05 -0600

    A bit of visual clean up

--- js/app.js
+++ js/app.js
@@ -1,7 +1,4 @@
-// Enemies our player must avoid
 var Enemy = function(x, y, speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
     this.x = x;
     this.y = y;
     this.speed = speed;
@@ -14,9 +11,6 @@ var Enemy = function(x, y, speed) {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
     this.x += this.speed * dt;
 
     if (this.x >= 505) {
